9c7e8f698bf037ad638a51623268a077d06ef376
Alhaytham <asalhaytham@gmail.com>
2018-02-13 19:27:47 +0000

    change the interface method signitures

--- src/main/java/services/databaseServices/CrudServices.java
+++ src/main/java/services/databaseServices/CrudServices.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public interface CrudServices {
 	public boolean createTable(String tableName, String properties);
-	public boolean interstIntoTable(String tableName, ArrayList<?> records);
-	public ArrayList<?> findAll(String tableName);
+	public boolean interstIntoTable(String tableName, ArrayList records);
+	public ArrayList findAll(String tableName);
 	public boolean dropTable(String tableName);
 }
