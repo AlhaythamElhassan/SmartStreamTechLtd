04df0deb86924af0574954bd37ed1064c21365cc
Alhaytham <asalhaytham@gmail.com>
2018-02-13 19:38:20 +0000

    run the service class

--- src/main/java/services/databaseServices/PersonsCrudServices.java
+++ src/main/java/services/databaseServices/PersonsCrudServices.java
@@ -22,11 +22,16 @@ public class PersonsCrudServices implements CrudServices {
 
 	public static void main(String[] args) {
 		PersonsCrudServices services = new PersonsCrudServices();
+		String inputDataFile = "src/main/resources/Person.data";
+		ReaderWriter fileReaderWriter = new ReaderWriter(inputDataFile);
+		ArrayList<Person> personRecords = fileReaderWriter.readFile();
+		
 		services.setDatabase(new Database());
 		// connect to the db 
 		services.getDatabase().connectToDb();
 		// get database statement
-		services.createTable("Persons","PersonId int, firstName varchar(20), lastName varchar(20), Street varchar(20), City varchar(20)");
+		services.createTable("Persons","PersonId int primary key, firstName varchar(20), lastName varchar(20), Street varchar(20), City varchar(20)");
+		//services.interstIntoTable("Persons", personRecords);
 		services.dropTable("Persons");
 		
 		
