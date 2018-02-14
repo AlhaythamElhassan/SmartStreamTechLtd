b903cf3c17ebe2f5db5eb3c96735fb2e6057a40b
Alhaytham <asalhaytham@gmail.com>
2018-02-14 13:25:04 +0000

    application is capable of inserting from Person.data and retrieve information

--- src/main/java/application/OrdersApplication.java
+++ src/main/java/application/OrdersApplication.java
@@ -2,7 +2,9 @@ package application;
 
 import java.util.ArrayList;
 
+import database.Database;
 import domain.Person;
+import services.databaseServices.PersonsCrudServices;
 import services.files.ReaderWriter;
 
 
@@ -11,11 +13,23 @@ public class OrdersApplication {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String dataInputFile = "src/main/resources/Person.data";
-		ReaderWriter fileReaderWriter = new ReaderWriter(dataInputFile);
+		PersonsCrudServices services = new PersonsCrudServices();
+		String inputDataFile = "src/main/resources/Person.data";
+		ReaderWriter fileReaderWriter = new ReaderWriter(inputDataFile);
 		ArrayList<Person> personRecords = fileReaderWriter.readFile();
-		for (Person person: personRecords)
-			System.out.println(person.toString());
+		ArrayList<Object> retrievedRecords; 
+		
+		services.setDatabase(new Database());
+		// connect to the db 
+		services.getDatabase().connectToDb();
+		// get database statement
+		services.createTable("Persons","PersonId int primary key, firstName varchar(20), lastName varchar(20), Street varchar(20), City varchar(20)");
+		services.interstIntoTable("Persons", personRecords);
+		retrievedRecords = services.findAll("Persons");
+		retrievedRecords.forEach(record -> {
+			System.out.println(record.toString());
+		});
+		services.dropTable("Persons");
 	}
 
 }
