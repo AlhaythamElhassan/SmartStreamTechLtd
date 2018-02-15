d2cb70f41aeafe23b46673c632ac95de7ed7b21e
AlhaythamElhassan <asalhaytham@gmail.com>
2018-02-15 11:57:04 +0000

    Feature/unit tests (#9)
    * linting and typos corrections
    
    * remove unused imports
    
    * correct typo
    
    * add project summary
    
    * comment and refactor

--- src/main/java/application/OrdersApplication.java
+++ src/main/java/application/OrdersApplication.java
@@ -3,39 +3,34 @@ package application;
 import java.util.ArrayList;
 
 import database.Database;
-import domain.Order;
-import domain.Person;
 import services.Filtering.FilteringServices;
-import services.databaseServices.PersonsCrudServices;
-import services.files.ReaderWriter;
-
+import services.databaseServices.CrudServices;
+import services.databaseServices.RecordsCrudServices;
+import services.files.FileReader;
 
 public class OrdersApplication {
-	/**
-	 * @param args
-	 */
+
 	public static void main(String[] args) {
-		PersonsCrudServices services = new PersonsCrudServices();
-		FilteringServices filteringServices = new FilteringServices(); 
+		CrudServices services = new RecordsCrudServices();
+		FilteringServices filteringServices = new FilteringServices();
 		String inputDataFile = "src/main/resources/Person.data";
-		ReaderWriter fileReaderWriter = new ReaderWriter(inputDataFile);
-		ArrayList<Person> personRecords = fileReaderWriter.readFile();
+		FileReader fileReaderWriter = new FileReader(inputDataFile);
+		ArrayList<Object> personRecords = fileReaderWriter.readFile();
 		fileReaderWriter.setFileName("src/main/resources/Order.data");
-		ArrayList<Order> ordersRecords = fileReaderWriter.readFile("\\|");
-		ArrayList<Object> retrievedPersonRecords,
-						retrievedOrderRecords,
-						personsWithAtLeastOneOrder,
-						ordersWithFirstNameOfCorrespondingPerson;
-		
+		ArrayList<Object> ordersRecords = fileReaderWriter.readFile("\\|");
+		ArrayList<Object> retrievedPersonRecords, retrievedOrderRecords, personsWithAtLeastOneOrder,
+		ordersWithFirstNameOfCorrespondingPerson;
+
 		services.setDatabase(new Database());
-		// connect to the db 
+		// connect to the database
 		services.getDatabase().connectToDb();
 		// create table Persons
 		System.out.println("########################################################");
 		System.out.println("Creating Tables");
 		System.out.println("########################################################");
-		services.createTable("Persons","PersonId int primary key, firstName varchar(20), lastName varchar(20), Street varchar(20), City varchar(20)");
-		// create table orders 
+		services.createTable("Persons",
+				"PersonId int primary key, firstName varchar(20), lastName varchar(20), Street varchar(20), City varchar(20)");
+		// create table orders
 		services.createTable("Orders", "ORDER_ID int primary key, ORDER_NO int, PERSON_ID int");
 		System.out.println("########################################################");
 		System.out.println("Finished creating tables");
@@ -54,7 +49,7 @@ public class OrdersApplication {
 		retrievedOrderRecords = services.findAll("Orders");
 		System.out.println("########################################################");
 		System.out.println("########################################################");
-		// Printing Person records 
+		// Printing Person records
 		retrievedPersonRecords.forEach(record -> {
 			System.out.println(record.toString());
 		});
@@ -65,8 +60,9 @@ public class OrdersApplication {
 		System.out.println("########################################################");
 		System.out.println("Persons with at least one Order");
 		System.out.println("########################################################");
-		personsWithAtLeastOneOrder = filteringServices.getPersonsWithAtLeastOneOrder(retrievedPersonRecords, retrievedOrderRecords);
-		if(personsWithAtLeastOneOrder != null) {
+		personsWithAtLeastOneOrder = filteringServices.getPersonsWithAtLeastOneOrder(retrievedPersonRecords,
+				retrievedOrderRecords);
+		if (personsWithAtLeastOneOrder != null) {
 			personsWithAtLeastOneOrder.forEach(person -> {
 				System.out.println(person.toString());
 			});
@@ -76,7 +72,8 @@ public class OrdersApplication {
 		System.out.println("########################################################");
 		System.out.println("All Orders with First Name of the corresponding person");
 		System.out.println("########################################################");
-		ordersWithFirstNameOfCorrespondingPerson = filteringServices.getOrdersWithFirstNameOfCorrespondingPerson(retrievedPersonRecords, retrievedOrderRecords);
+		ordersWithFirstNameOfCorrespondingPerson = filteringServices
+				.getOrdersWithFirstNameOfCorrespondingPerson(retrievedPersonRecords, retrievedOrderRecords);
 		if (!ordersWithFirstNameOfCorrespondingPerson.isEmpty()) {
 			ordersWithFirstNameOfCorrespondingPerson.forEach(order -> {
 				System.out.println(order.toString());
@@ -94,5 +91,3 @@ public class OrdersApplication {
 	}
 
 }
-
-
--- src/main/java/services/files/FileReader.java
+++ src/main/java/services/files/FileReader.java
@@ -7,65 +7,58 @@ import java.util.Scanner;
 
 import domain.Order;
 import domain.Person;
- 
 
-public class ReaderWriter {
-	
+/**
+ * This component is responsible from reading data from files Person.data and
+ * Order.data
+ * 
+ * @author alhaytham
+ *
+ */
+public class FileReader {
+
 	private String fileName;
 
+	public static void main(String[] args) {
+		String inputDataFile = "src/main/resources/Person.data";
+		FileReader fileReaderWriter = new FileReader(inputDataFile);
+		ArrayList<Object> personRecords = fileReaderWriter.readFile();
+		for (Object person : personRecords)
+			System.out.println(person.toString());
+	}
 
-
-	public ReaderWriter(String inputFile) {
+	public FileReader(String inputFile) {
 		super();
 		setFileName(inputFile);
 	}
 
-
-
-	public ReaderWriter() {
+	public FileReader() {
 		super();
 	}
-
-
 
 	public String getFileName() {
 		return fileName;
 	}
 
-
-
 	public void setFileName(String fileName) {
 		this.fileName = fileName;
 	}
 
-
-
 	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		String inputDataFile = "src/main/resources/Person.data";
-		ReaderWriter fileReaderWriter = new ReaderWriter(inputDataFile);
-		ArrayList<Person> personRecords = fileReaderWriter.readFile();
-		for (Person person: personRecords)
-			System.out.println(person.toString());
-	}
-	/** 
 	 * @return ArrayList of records found in the file
 	 */
-	public ArrayList<Person> readFile() {
-		ArrayList<Person> records = new ArrayList<Person>();
+	public ArrayList<Object> readFile() {
+		ArrayList<Object> records = new ArrayList<Object>();
 		Scanner in;
-		try { 	
-			in = new Scanner(
-					new FileInputStream(this.fileName), "UTF-8");
+		try {
+			in = new Scanner(new FileInputStream(this.fileName), "UTF-8");
 			in.nextLine();
-			while(in.hasNext()){
+			while (in.hasNext()) {
 				Person personRecord = mapLineToRecord(in.nextLine());
 				if (personRecord != null)
 					records.add(personRecord);
 			}
-			// close Scanner 
+			// close Scanner
 			in.close();
 		} catch (FileNotFoundException e) {
 			System.out.println("Error reading File " + this.fileName);
@@ -74,25 +67,28 @@ public class ReaderWriter {
 		return records;
 	}
 
+	/**
+	 * Map a given line using , as a delimiter to fields
+	 * 
+	 * @param line
+	 *            is a string that represent the record
+	 * @return Person an instant of type Person or null
+	 */
 	public Person mapLineToRecord(String line) {
 		String[] fields = line.split("\\,");
-		if(fields.length > 1){
-			Person person = new Person(Integer.parseInt(fields[0]),
-					fields[1],
-					fields[2],
-					fields[3],
-					fields[4]);
+		if (fields.length > 1) {
+			Person person = new Person(Integer.parseInt(fields[0]), fields[1], fields[2], fields[3], fields[4]);
 			return person;
 		}
 		return null;
 	}
-	
-	public ArrayList<Order> readFile(String delimiter) {
-		ArrayList<Order> records = new ArrayList<Order>();
+
+	public ArrayList<Object> readFile(String delimiter) {
+		ArrayList<Object> records = new ArrayList<Object>();
 		try {
 			Scanner in = new Scanner(new FileInputStream(this.fileName), "UTF-8");
 			in.nextLine();
-			while(in.hasNext()) {
+			while (in.hasNext()) {
 				Order orderRecord = mapLineToRecord(in.nextLine(), delimiter);
 				if (orderRecord != null)
 					records.add(orderRecord);
@@ -103,14 +99,20 @@ public class ReaderWriter {
 			e.printStackTrace();
 		}
 		return records;
-		
+
 	}
 
-
+	/**
+	 * Map a given line using a given delimiter to an Order
+	 * 
+	 * @param line
+	 *            is a string that represent the record
+	 * @return Order an instance of type Order or null
+	 */
 
 	private Order mapLineToRecord(String line, String delimiter) {
 		String[] fields = line.split(delimiter);
-		if(fields.length > 1) {
+		if (fields.length > 1) {
 			return new Order(Integer.parseInt(fields[0]), Integer.parseInt(fields[1]), Integer.parseInt(fields[2]));
 		}
 		return null;
